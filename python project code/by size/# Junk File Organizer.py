6dff643ef2ea768355fc3902b4dd1790217c5615
vicky <sadhuvivek123@gmail.com>
2021-08-03 13:27:55 +0530

    python project

---
+++ python project code/by size/# Junk File Organizer.py
@@ -0,0 +1,96 @@
+# Junk File Organizer
+# Python Project To Sort Files 
+
+
+import os, shutil
+
+print("Welcome To Junk File Organizer")
+print("Program By - Vivek Sadhu")
+print("--OPTIONS--")
+print("Enter 1 For File Extension Sorting")
+print("Enter 2 For File Size Sorting")
+typeofsort = input("Enter Sorting Method: ")
+folderpath = input('Enter Your Folder Path To Sort The Files: ')
+
+
+# To Sort According To Extension
+
+file_extensions={
+       'Audio':('.mp3','.wav','.flac', '.m4a', '.aac'),
+       'Video':('.mp4','.mkv','.MKV','.flv','.mpeg'),
+       'Images':('.jpeg', '.jpg', '.tiff', '.gif', '.png'),
+       'Docs':('.doc','.pdf','.txt','.docx','.xls', '.xlsx', '.ppt', '.pptx', '.xps'),
+       'Archives':('.zip', '.7z', '.rar'),
+       'Others':('.exe', '.apk', '.bat', '.bin'),
+       'Programming':('.py', '.htm', '.html', '.html5', '.css', '.php', '.js')
+}
+new_path={
+    "Organized":('Audio','Video','Images','Docs','Archives','Others','Programming')
+}
+
+if typeofsort=="1":
+    def file_finder(folderpath,file_extensions):
+        files=[]
+        for file in os.listdir(folderpath):
+            for extension in file_extensions:
+                if file.endswith(extension):
+                    files.append(file)
+        return files 
+
+    for extensions_type,extension_tuple in file_extensions.items():
+        folder_name=extensions_type.split('_')[0]
+        folder_path=os.path.join(folderpath,folder_name)
+        os.mkdir(folder_path)
+        for item in (file_finder(folderpath,extension_tuple)):
+            item_path=os.path.join(folderpath,item)
+            item_new_path=os.path.join(folder_path,item)
+            shutil.move(item_path,item_new_path)
+
+    for extensions_type,extension_tuple in new_path.items():
+        folder_name=extensions_type.split('_')[0]
+        folder_path=os.path.join(folderpath,folder_name)
+        os.mkdir(folder_path)
+        for item in (file_finder(folderpath,extension_tuple)):
+            item_path=os.path.join(folderpath,item)
+            item_new_path=os.path.join(folder_path,item)
+            shutil.move(item_path,item_new_path)
+    print("Done Sorting :)")
+
+
+# To Sort According To Size
+
+if typeofsort=="2":
+    def sizecheck(folderpath):
+        list_dir=os.walk(folderpath)
+        for dir,filename, file in list_dir:
+            for f in file:
+                sizeoffile=os.stat(dir+"/"+f).st_size
+                try:
+                    if sizeoffile < 1024:
+                        if os.path.exists(folderpath+"/Byte_Files/"):
+                            shutil.move(folderpath+"/"+f, folderpath+"/Byte_Files/"+f)
+                        else:
+                            os.mkdir(folderpath+"/Byte_Files/")
+                            shutil.move(folderpath+"/"+f, folderpath+"/Byte_Files/"+f)
+                    elif sizeoffile >= 1024 and sizeoffile < 1000*1024:
+                        if os.path.exists(folderpath+"/KiloBytes_Files/"):
+                            shutil.move(folderpath+"/"+f, folderpath+"/KiloBytes_Files/"+f)
+                        else:
+                            os.mkdir(folderpath+"/KiloBytes_Files/")
+                            shutil.move(folderpath+"/"+f, folderpath+"/KiloBytes_Files/"+f)
+                    elif sizeoffile >= 1000*1024 and sizeoffile <= 1000*1024*1024:
+                        if os.path.exists(folderpath+"/MegaBytes_Files/"):
+                            shutil.move(folderpath+"/"+f, folderpath+"/MegaBytes_Files/"+f)
+                        else:
+                            os.mkdir(folderpath+"/MegaBytes_Files/")
+                            shutil.move(folderpath+"/"+f, folderpath+"/MegaBytes_Files/"+f)
+                    else:
+                        if os.path.exists(folderpath+"/GigaBytes_Files/"):
+                            shutil.move(folderpath+"/"+f, folderpath+"/GigaBytes_Files/"+f)
+                        else:
+                            os.mkdir(folderpath+"/GigaBytes_Files/")
+                            shutil.move(folderpath+"/"+f, folderpath+"/GigaBytes_Files/"+f)
+                except FileExistsError:
+                    continue
+        print("Done Sorting :)")
+    sizecheck(folderpath)
---
+++ python project code/readme.md
@@ -0,0 +1,49 @@
+BATCH – SABRAHMANYAN
+
+ 
+
+
+
+
+
+
+
+
+
+ 
+INTRODUCTION:-
+The aim of this project is to build a python program that runs as a Command-line tool. Basically, as a lazy programmer, my desktop Is full of files as shown below. Due to a large number of files, it is A daunting task to sit and organize each file. To make that task Easy, we need a Python script that comes in handy and returns a Folder where all the files are organized in a well-manner within Seconds. It should organize by size, last used or even extensions. So below is a python program to organize or simplify everything in appropriate folder in the single go and remove empty directories.
+
+What do it do?
+❖ Organize by extension 
+❖ Organize by size
+ ❖ Organize by Date and time
+
+
+How it appears?
+❖ Whenever the user selects the option, the files will be organized based on the option selected by the user. 
+❖ Below is the given scenario for which how they wanted to organize like.
+ 
+
+ORGANIZE BY EXTENSION:- 
+❖ By using this option user can organize their files by their file extension in a given folder, folder will be created according to file extension and finally all files will be moved to a created folder.
+
+ORGANIZE BY SIZE:- 
+❖ By using this option user can organize their files by their file size, according to file sizes random folders will be created and respective files will be moved to them.
+ORGANIZE BY LAST USED/ACCESSED DATE:- 
+❖By using this option user can organize their files by last used date. Random folders will be created according to files last used date and files will be moved to them.
+NOTE:-  ❖If user not provide any think or user provide wrong path than program will tell to provide valid thing.
+Before file execution, files look like:-
+
+
+ 
+
+After file execution, files look like:-
+
+ 
+
+At a glance:-
+
+ ❖ I had used Shuttle/ Os modules for the file arrangement, Date and time to know when the files were created and many more built-in functions.
+ ❖ I have a directory path where lots of files of different types are present (like above) and my program segregate each file type into their respective folders. 
+❖ As a future scope of this project, we can design the ui for the program so that a normal user can easily interact with it. We can add more features like deleting the junk files after a certain period of time
